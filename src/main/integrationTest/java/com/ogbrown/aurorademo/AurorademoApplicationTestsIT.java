d2de8ae89d75fe5ad4c2e469827c98c4d3bda43b
O.G. Brown, III <ogbrown3@gmail.com>
2025-07-16 06:54:54 -0400

    Initial Spring Boot commit

---
+++ src/main/integrationTest/java/com/ogbrown/aurorademo/AurorademoApplicationTestsIT.java
@@ -0,0 +1,13 @@
+package com.ogbrown.aurorademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AurorademoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/main/java/com/ogbrown/aurorademo/AurorademoApplication.java
@@ -0,0 +1,13 @@
+package com.ogbrown.aurorademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurorademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AurorademoApplication.class, args);
+	}
+
+}
