3195af77dfafd7377eee082e723cb32ad3e82883
O.G. Brown, III <ogbrown3@gmail.com>
2025-07-16 07:49:01 -0400

    add application-local

--- src/main/java/com/ogbrown/aurorademo/AurorademoApplication.java
+++ src/main/java/com/ogbrown/aurorademo/AurorademoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AurorademoApplication {
+public class AurorademoApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(AurorademoApplication.class, args);
---
+++ src/test/java/com/ogbrown/aurorademo/AurorademoApplicationTest.java
@@ -0,0 +1,17 @@
+package com.ogbrown.aurorademo;
+
+import org.junit.jupiter.api.Test;
+
+
+public class AurorademoApplicationTest{
+
+	@Test
+	public void tryMyTest() {
+		// This is a test method to ensure the application context loads correctly.
+		// It can be used to verify that the application starts without any issues.
+		// You can add assertions here to check specific beans or configurations if needed.
+		System.out.println("Test method executed successfully.");
+
+	}
+
+}
